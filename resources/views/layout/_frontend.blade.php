cf93e0cb1d9cc96b7bbd5d385985a986d9564917
nhoxzunbmt <nhoxzunbmt@gmail.com>
2015-12-30 13:49:08 +0700

    Auto Update

--- resources/views/layout/_frontend.blade.php
+++ resources/views/layout/_frontend.blade.php
@@ -30,7 +30,7 @@
 @section('afterBody')
 @show
 @section('head_js')
-    <script type='text/javascript' src='{{ asset('js/jquery.js') }}'></script>
+    <script type='text/javascript' src='{{ asset('js/jquery-1.11.3.min.js') }}'></script>
 <script type='text/javascript' src='{{ asset('js/migrate.js') }}'></script>
 <script type='text/javascript' src="{{ asset('js/seven.min.js') }}"></script>
 <script type='text/javascript' src="{{ asset('js/owl.carousel.min.js') }}"></script>
