c6cc36247fb51f330793fb2d43f1da2e0ac33e09
nhoxzunbmt <nhoxzunbmt@gmail.com>
2015-12-30 14:01:28 +0700

    Auto Update

--- resources/views/layout/_frontend.blade.php
+++ resources/views/layout/_frontend.blade.php
@@ -3,23 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>@section('title') @show</title>
-    <meta name="description" content="{{ isset($description) ? $description : 'Blog' }}"/>
-    <meta name="keywords" content="{{ Cache::get('website_keywords') }}"/>
-    <meta name="author" content="{{ Cache::get('system_author_website') }}"/>
+    @section('meta')
+    @show
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"/>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    @section('css')
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('css/icons.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('css/animate.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('css/responsive.css') }}" rel="stylesheet" type="text/css"/>
-    @section('beforeStyle')
-    @show
-    @section('head_style')
-    @show
-    @section('afterStyle')
     @show
 </head>
 <body @section('body_attr')class=""@show>
@@ -29,7 +24,7 @@
 @show
 @section('afterBody')
 @show
-@section('head_js')
+@section('js')
     <script type='text/javascript' src='{{ asset('js/jquery-1.11.3.min.js') }}'></script>
 <script type='text/javascript' src='{{ asset('js/migrate.js') }}'></script>
 <script type='text/javascript' src="{{ asset('js/seven.min.js') }}"></script>
@@ -39,9 +34,6 @@
 <script type='text/javascript' src="{{ asset('plugins/prettyphoto/js/jquery.prettyPhoto.js') }}"></script>
 <script type='text/javascript' src="{{ asset('js/custom.js') }}"></script>
 <script type='text/javascript' src="{{ asset('js/skin.js') }}"></script>
-@parent
-@stop
-@section('head_js')
 @show
 </body>
 </html>
