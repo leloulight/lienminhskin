9e4b503dd308908f3d70d3bb0ded03d349104f5f
nhoxzunbmt <nhoxzunbmt@gmail.com>
2015-12-30 13:41:49 +0700

    Auto Update

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -3,7 +3,7 @@
 class="home page page-id-39 page-template page-template-page-home-slideshow page-template-page-home-slideshow-php _masterslider _msp_version_2.7.2 wpb-js-composer js-comp-ver-4.3.4 vc_responsive"
 @stop
 @section('body')
-<div id="layout" class="boxed-margin ">
+    <div id="layout" class="boxed-margin">
     @include('widgets.header')
     @include('widgets.slider')
     @include('widgets.footer')
