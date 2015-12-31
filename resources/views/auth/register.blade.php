a6bfd900a9354588f202157d00587042cdb23f4e
nhoxzunbmt <nhoxzunbmt@gmail.com>
2015-12-31 16:44:04 +0700

    Auto Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,11 @@
 <!-- resources/views/auth/register.blade.php -->
-
+@if (count($errors) > 0)
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{!! $error !!}</li>
+        @endforeach
+    </ul>
+@endif
 <form method="POST" action="{{ url('/auth/register') }}">
     {!! csrf_field() !!}
 
