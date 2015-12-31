b385ae11bc50b20820ec8d02bf1464d2067ef9a7
nhoxzunbmt <nhoxzunbmt@gmail.com>
2015-12-31 15:34:01 +0700

    Auto Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <!-- resources/views/auth/register.blade.php -->
 
-<form method="POST" action="/auth/register">
+<form method="POST" action="{{ url('/auth/register') }}">
     {!! csrf_field() !!}
 
     <div class="col-md-6">
